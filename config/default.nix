d5d4ae9d8f61bde85fc3d1e4577fb6e00f46ef71
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2024-06-15 12:25:30 +0630

    feat: add telescope nvim

--- config/default.nix
+++ config/default.nix
@@ -1,14 +1,15 @@
 {
   # Import all your configuration modules here
   imports = [ 
-  ./bufferline.nix
-  ./catppuccin.nix
-  ./cmp.nix
-  ./key-maps.nix
-  ./lsp.nix
-  ./lualine.nix
-  ./neo-tree.nix
-  ./options.nix
-  ./treesitter.nix
+    ./bufferline.nix
+    ./catppuccin.nix
+    # ./cmp.nix
+    ./key-maps.nix
+    ./lsp.nix
+    ./lualine.nix
+    ./neo-tree.nix
+    ./options.nix
+    ./telescope.nix
+    ./treesitter.nix
   ];
 }
