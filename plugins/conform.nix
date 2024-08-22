244275f6398574baace62a1439675e8281094713
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2024-08-22 08:17:08 +0630

    feat: add work version of conform config

--- plugins/conform.nix
+++ plugins/conform.nix
@@ -1,14 +1,52 @@
-{
+{ pkgs, helpers, ... }: {
   plugins.conform-nvim = {
     enable = true;
+    notifyOnError = true;
 
-    formatOnSave = true;
+    formatters = {
+      stylua = {
+        command = "${pkgs.stylua}/bin/stylua";
+      };
+      nixfmt = {
+        command = "${pkgs.nixfmt-rfc-style}/bin/nixfmt";
+      };
+      biome = {
+        command = "${pkgs.biome}/bin/biome";
+      };
+      black = {
+        command = "${pkgs.black}/bin/black";
+      };
+    };
 
     formattersByFt = {
+      javascript = (helpers.listToUnkeyedAttrs [ "prettierd" "prettier" "biome" ]) // { stopAfterFirst = true; };
+      typescript = (helpers.listToUnkeyedAttrs [ "prettierd" "prettier" "biome" ]) // { stopAfterFirst = true; };
+      javascriptreact = (helpers.listToUnkeyedAttrs [ "prettierd" "prettier" "biome" ]) // { stopAfterFirst = true; };
+      typescriptreact = (helpers.listToUnkeyedAttrs [ "prettierd" "prettier" "biome" ]) // { stopAfterFirst = true; };
+
+      go = [ "gofmt" ];
       lua = [ "stylua" ];
-      python = [ [ "isort" "black" ] ];
-      javascript = [ [ "biome" "prettierd" "prettier" ] ];
-      rust = [ "rust-analyzer" ];
+      nix = [ "nixfmt" ];
+      python = [ "black" ];
+      rust = [ "rustfmt" ];
+    };
+
+    formatOnSave = {
+      timeoutMs = 500;
+      lspFallback = true;
     };
   };
+
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>lf";
+      action = "<cmd>lua require('conform').format()<cr>";
+      options = {
+        desc = "Format";
+        silent = true;
+      };
+    }
+  ];
+
 }
