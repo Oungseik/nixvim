6d7f1d7160361f297354559aad304b6f088f1bb8
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2024-08-22 21:52:43 +0630

    configure bufferline

--- plugins/editor/lualine.nix
+++ plugins/editor/lualine.nix
@@ -30,47 +30,23 @@
           name = "branch";
           icon = "";
         }
-        # {
-        #   name = "diff";
-        #   extraConfig = {
-        #     symbols = {
-        #       added = " ";
-        #       modified = " ";
-        #       removed = " ";
-        #     };
-        #   };
-        # }
       ];
 
       lualine_c = [
         {
-          fmt.__raw = ''
-            function()
-              local buf_clients = vim.lsp.get_clients { bufnr = 0 }
-              if #buf_clients == 0 then
-                return "LSP Inactive"
-              end
-
-              local buf_ft = vim.bo.filetype
-              local buf_client_names = {}
-
-              -- add client
-              for _, client in pairs(buf_clients) do
-                if client.name ~= "null-ls" and client.name ~= "copilot" then
-                  table.insert(buf_client_names, client.name)
-                end
-              end
-
-              local unique_client_names = table.concat(buf_client_names, ", ")
-              local language_servers = string.format("  %s", unique_client_names)
-
-              return language_servers
-            end,
-          '';
-          color = {
-            fg = "#c6a0f6";
+          name = "diff";
+          extraConfig = {
+            symbols = {
+              added = " ";
+              modified = " ";
+              removed = " ";
+            };
           };
         }
+      ];
+
+      # right 
+      lualine_x = [
         {
           name = "diagnostics";
           extraConfig = {
@@ -81,27 +57,6 @@
               info = " ";
               hint = "󰝶 ";
             };
-          };
-        }
-      ];
-
-      # right 
-      lualine_x = [
-        {
-          name = "filetype";
-          extraConfig = {
-            icon_only = true;
-            separator = "";
-            padding = {
-              left = 1;
-              right = 0;
-            };
-          };
-        }
-        {
-          name = "filename";
-          extraConfig = {
-            path = 0;
           };
         }
         { name = "navic"; }
@@ -121,9 +76,31 @@
         }
       ];
 
-      lualine_y = [
-        { name = "progress"; }
-      ];
+      lualine_y = [{
+        fmt.__raw = ''
+          function()
+            local buf_clients = vim.lsp.get_clients { bufnr = 0 }
+            if #buf_clients == 0 then
+              return "LSP Inactive"
+            end
+
+            local buf_ft = vim.bo.filetype
+            local buf_client_names = {}
+
+            -- add client
+            for _, client in pairs(buf_clients) do
+              if client.name ~= "null-ls" and client.name ~= "copilot" then
+                table.insert(buf_client_names, client.name)
+              end
+            end
+
+            local unique_client_names = table.concat(buf_client_names, ", ")
+            local language_servers = string.format("  %s", unique_client_names)
+
+            return language_servers
+          end,
+        '';
+      }];
       lualine_z = [{ name = "location"; }];
     };
   };
